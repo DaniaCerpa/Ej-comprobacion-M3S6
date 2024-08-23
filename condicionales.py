2d508bd46347ec0b630ae06a6f2c5096dbcc4af1
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-22 20:34:11 -0400

    Tercer commit

--- condicionales.py
+++ condicionales.py
@@ -1,17 +1,18 @@
-n = int(input("Selecciona un numero: \n"))
+numero = int(input("Selecciona un numero: \n"))
 
 
-if n % 2 == 0:
-    if n > 0:
+if numero % 2 == 0:
+    if numero > 0:
         print ("numero par positivo")
     else:
         print ("numero par negativo")
-elif n == 0:
+elif numero == 0:
     print ("numero cero")
-elif  n % 2 != 0: #esta linea podria ser remplazada con un else sin condición y funcionaria de igual modo.
-    if n > 0:
+elif  numero % 2 != 0: #esta linea podria ser remplazada con un else sin condición y funcionaria de igual modo.
+    if numero > 0:
         print ("numero impar positivo")   
     else: 
         print("numero impar negativo")
+
 
 
